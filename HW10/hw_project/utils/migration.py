ff768ddb834fe3fb9f155159a5a6bda14999a45e
Y3vh3n11 <145870816+Y3vh3n11@users.noreply.github.com>
2024-05-05 18:08:22 +0300

    s

--- HW10/hw_project/utils/migration.py
+++ HW10/hw_project/utils/migration.py
@@ -12,19 +12,15 @@ django.setup()
 
 
 def get_mongodb():
-    # config = configparser.ConfigParser()
-    # config.read('config.ini')
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-    # mongo_user = config.get('DB', 'USER')
-    # mongodb_pass = config.get('DB', 'PASS')
-    # db_name = config.get('DB', 'DB_NAME')
-    # domain = config.get('DB', 'DOMAIN')
+    mongo_user = config.get('DB', 'USER')
+    mongodb_pass = config.get('DB', 'PASS')
+    db_name = config.get('DB', 'DB_NAME')
+    domain = config.get('DB', 'DOMAIN')
 
-    mongo_user = 'codepraktik'
-    mongodb_pass = 'it9AktJTWuXbqazI'
-    db_name = 'HW10'
-    domain = 'cluster0.esxaixs.mongodb.net'
-
+ 
     uri = f'mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority'
 
     client = MongoClient(uri, server_api=ServerApi('1'))
